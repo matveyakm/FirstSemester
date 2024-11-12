2ef37fd027e8bb42d48c8a9a2979a64f02de3063
matveyakm <matveyakm@gmail.com>
2024-11-13 01:33:38 +0300

    minor fixes. added test

--- HW7/countingRhyme/main.c
+++ HW7/countingRhyme/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "../../localLibs/list/cyclicList.h"
+#define TEST_FAILED 52
 
 int findK(int n, int m) {
     Cyclic *warriors = createCyclic(0);
@@ -9,26 +10,46 @@ int findK(int n, int m) {
     }
     int i = 0;
     int chainCounter = 1;
-    printCyclic(warriors); //
     while (cyclicLength(warriors) > 1) {
         if (chainCounter % m == 0) {
-            //printf("DEL %d (%d)\n", i, cyclicLength(warriors));
             popAtC(warriors, i);
-            printCyclic(warriors); //
             --i;
             chainCounter = 1;
         } else {
             ++chainCounter;
         }
-        ++i;
+        i = (i + 1) % cyclicLength(warriors);
     }
     int k = peekC(warriors, 0);
     clearCyclic(warriors);
     return k;
 }
 
+bool validationTest(void) {
+    bool isTestSuccesful = true;
+    isTestSuccesful *= findK(5, 3) == 4;
+    isTestSuccesful *= findK(13, 3) == 12;
+    isTestSuccesful *= findK(7, 2) == 6;
+    isTestSuccesful *= findK(11, 4) == 8;
+    isTestSuccesful *= findK(6, 4) == 4;
+    isTestSuccesful *= findK(11, 3) == 6;
+    isTestSuccesful *= findK(9, 8) == 2;
+    isTestSuccesful *= findK(9, 1) == 8;
+    return isTestSuccesful;
+}
+
 int main() {
-    int n = 5;
-    int m = 3;
-    printf("%d\n", findK(n, m));
+    #ifdef TEST
+    if (!validationTest()) {
+        puts("Test failed.");
+        return TEST_FAILED;
+    } else {
+        puts("Ready to process.");
+    }
+    #endif
+
+    int n, m;
+    puts("Enter n, m:");
+    scanf("%d %d", &n, &m);
+    printf("k = %d\n", findK(n, m));
 }
