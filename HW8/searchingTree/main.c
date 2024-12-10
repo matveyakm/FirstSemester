30e6b1262988c50bfff369f689a40d35f32dedcb
matveyakm <matveyakm@gmail.com>
2024-12-10 20:47:09 +0300

    removing excess

--- HW8/searchingTree/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "dictionary.h"
-
-int main() {
-    Dictionary *dict = createDictionary();
-    addPairToDict(dict, 10, "Lada");
-    addPairToDict(dict, 9, "Kama");
-    printDictionary(dict);
-}
